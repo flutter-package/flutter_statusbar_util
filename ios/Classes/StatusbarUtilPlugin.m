42f2e876d8693be6844aa405342b055948458e8f
chaves <mail@chavesgu.com>
2019-12-05 17:43:53 +0800

    complete

--- ios/Classes/StatusbarUtilPlugin.m
+++ ios/Classes/StatusbarUtilPlugin.m
@@ -2,9 +2,11 @@
 
 @implementation StatusbarUtilPlugin
 + (void)registerWithRegistrar:(NSObject<FlutterPluginRegistrar>*)registrar {
-      FlutterMethodChannel *channel = [FlutterMethodChannel methodChannelWithName:@"statusbar_util" binaryMessenger:[registrar messenger]];
-      StatusbarUtilPlugin *instance = [[StatusbarUtilPlugin alloc] init];
-      [registrar addMethodCallDelegate:instance channel:channel];
+      FlutterMethodChannel* channel = [FlutterMethodChannel
+            methodChannelWithName:@"statusbar_util"
+                  binaryMessenger:[registrar messenger]];
+        StatusbarUtilPlugin* instance = [[StatusbarUtilPlugin alloc] init];
+        [registrar addMethodCallDelegate:instance channel:channel];
 }
 
 - (void)handleMethodCall:(FlutterMethodCall *)call result:(FlutterResult)result {
